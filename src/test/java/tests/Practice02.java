1f0bb216032366d5732f4821b81703f73e5214a8
Erol Evren <100449806+erlevren@users.noreply.github.com>
2023-02-23 20:30:51 +0300

    V2

--- src/test/java/tests/Practice02.java
+++ src/test/java/tests/Practice02.java
@@ -1,6 +1,15 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.HerokuPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class Practice02 {
+
+
     /*
     Negative Test
     1.  https://id.heroku.com/login sayfasina gidin
@@ -10,4 +19,25 @@ public class Practice02 {
     5.  sayfalari kapatiniz
 
  */
+    @DataProvider
+    public static Object[][] kullanici() {
+        return new Object[][]{{ConfigReader.getProperty("email1"),ConfigReader.getProperty("password1")},
+                {ConfigReader.getProperty("email2"),ConfigReader.getProperty("password2")},
+                {ConfigReader.getProperty("email3"),ConfigReader.getProperty("password3")}};
+    }
+
+    @Test (dataProvider = "kullanici")
+    public void testName(String email,String password) throws InterruptedException {
+        Driver.getDriver().get(ConfigReader.getProperty("herokuUrl"));
+        HerokuPage obj =new HerokuPage();
+        obj.email.sendKeys(email);
+        Thread.sleep(2000);
+        obj.password.sendKeys(password);
+        Thread.sleep(2000);
+        obj.loginButton.click();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(obj.yazi.isDisplayed());
+        Driver.closeDriver();
+        softAssert.assertAll();
+    }
 }
